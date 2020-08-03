01731d209bc2b1df38f19ea64ea0b4d72d0b768f
Christie Yoon <cleex1999@gmail.com>
2020-08-03 17:06:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 require "pry"
 binding.pry
 def my_each(collection) # put argument(s) here\
-  if my_each.length >= 1
-      i = 0
+  #if my_each.length >= 1
+      #i = 0
     while counter < collection.length
     my_each(collection) do |i|
       yield do
