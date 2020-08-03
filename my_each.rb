ac3ff3f12517ef06aa7aab8ffdb5e218acd4f1c3
Christie Yoon <cleex1999@gmail.com>
2020-08-03 17:09:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -5,8 +5,9 @@ def my_each(collection) # put argument(s) here\
       i = 0
     while i < collection.length
     my_each(collection) do |i|
-      yielding do
+      yielding(collection[i]) do
        puts "i"
+       
       i = i + 1
     end
     my_each
