f84992f30a8ff3c3333d62484656cca983acfd74
Christie Yoon <cleex1999@gmail.com>
2020-08-03 17:03:11 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
-  # code here
-end
+require "pry"
+binding.pry
+def my_each(words) # put argument(s) here\
+  if my_each.length >= 1
+      i = 0
+    while counter < words.length
+    my_each(words) do |words|
+      yield(words[i])
+      puts i
+      i = i + 1
+    end
+    my_each
+  end
+  end
