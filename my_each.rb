cc37502168b01031912a5fea6e1ef89f728e68c5
Christie Yoon <cleex1999@gmail.com>
2020-08-03 17:16:25 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,13 @@
-#require "pry"
-#binding.pry
+require "pry"
+binding.pry
 #collection = [1, 2, 3, 4]
 def my_each(collection) # put argument(s) here\
   #if my_each.length >= 1
-      i = 0
+       i = 0
     while i < collection.length
-      yield(collection[i]) do
+      yielding(collection[i]) do
        puts "i"
-      i += 1
+       i += 1
     end
     puts collection
   end
